f7c86fbb63b1381d2d106c34900519b5fbcc968d
3dev <121336945+suttiphod1234@users.noreply.github.com>
2026-05-03 10:54:46 +0700

    updater352

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // allow frontend to connect
-  await app.listen(process.env.PORT ?? 5001);
+  await app.listen(process.env.PORT ?? 5005, '127.0.0.1');
 }
 void bootstrap();
